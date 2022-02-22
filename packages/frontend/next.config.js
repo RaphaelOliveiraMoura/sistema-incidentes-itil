cb99beea5e7d2152a78726b49fb73eb3c48dbafc
RaphaelOliveiraMoura <raphael.oliveira@natahouse.com>
2022-02-22 15:31:17 -0300

    chore(front): add eslint + prettier setup

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['.', '.eslintrc.json', '.prettierrc.json', 'tsconfig.json']
+  },
   compiler: {
     styledComponents: true,
-  },
+  }
 }
 
 module.exports = nextConfig
