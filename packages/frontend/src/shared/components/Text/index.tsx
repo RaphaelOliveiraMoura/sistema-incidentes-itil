60d0defce8087b30983885c2a668cf5a12b5266d
RaphaelOliveiraMoura <raphael.oliveira@natahouse.com>
2022-02-22 21:15:13 -0300

    feat(front): start creating text component

---
+++ packages/frontend/src/shared/components/Text/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import * as S from './styles'
+import { ValidHtmlTextTags } from './types'
+
+export type TextVariants = 'default' | 'title' | 'subtitle'
+
+export type TextProps = {
+  variant?: TextVariants
+}
+
+type TagsMap = { [key in TextVariants]: ValidHtmlTextTags }
+
+const tagsMap: TagsMap = {
+  default: 'p',
+  title: 'h1',
+  subtitle: 'h2'
+}
+
+export const Text: React.FC<TextProps> = ({
+  variant = 'default',
+  children
+}) => {
+  const textHtmlTag = tagsMap[variant]
+
+  return <S.Wrapper as={textHtmlTag}>{children}</S.Wrapper>
+}
